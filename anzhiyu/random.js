bc173f9a64bbbe48b398a602177fd4d8396d2325
YuejinWu <2099212070@qq.com>
2025-12-09 02:54:28 +0800

    Site updated: 2025-12-09 02:54:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/08/AIpiatform_技术栈/","2025/12/06/THE FIRST/","2025/12/06/hello-world/","2025/12/07/English/","2025/12/09/字体测试/"];function toRandomPost(){
+var posts=["2025/12/07/English/","2025/12/08/AIpiatform_技术栈/","2025/12/06/THE FIRST/","2025/12/09/字体测试/","2025/12/06/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
