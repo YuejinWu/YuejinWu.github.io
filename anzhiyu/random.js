4e79d80095d7b733a3928375b899a48aac29d605
YuejinWu <2099212070@qq.com>
2025-12-23 23:16:49 +0800

    Site updated: 2025-12-23 23:16:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/12/07/English/","2025/12/22/DB_SQL 语言简读/","2025/12/22/DB_关系模型简读/","2025/12/08/AIpiatform_技术栈/","2025/12/16/RNN笔记/","2025/12/06/hello-world/","2025/12/06/THE FIRST/","2025/12/10/闲言碎语2/","2025/12/16/self-attention简读/","2025/12/09/个人介绍/"];function toRandomPost(){
+var posts=["2025/12/08/AIpiatform_技术栈/","2025/12/22/DB_关系模型简读/","2025/12/22/DB_SQL 语言简读/","2025/12/07/English/","2025/12/16/RNN笔记/","2025/12/06/THE FIRST/","2025/12/06/hello-world/","2025/12/10/闲言碎语2/","2025/12/16/self-attention简读/","2025/12/09/个人介绍/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
